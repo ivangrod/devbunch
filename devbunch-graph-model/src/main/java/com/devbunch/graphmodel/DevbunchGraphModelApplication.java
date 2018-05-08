2052fc9d62b7d5c1bd37f95a96039a23f8c334a8
ivangrod <ivangutrod@gmail.com>
2018-05-08 08:25:52 +0200

    Added docker folder: ELK stack, PostgreSQL, PgAdmin, Kafka stack

---
+++ devbunch-graph-model/src/main/java/com/devbunch/graphmodel/DevbunchGraphModelApplication.java
@@ -0,0 +1,12 @@
+package com.devbunch.graphmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevbunchGraphModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevbunchGraphModelApplication.class, args);
+	}
+}
