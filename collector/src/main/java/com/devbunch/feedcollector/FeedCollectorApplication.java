80e36b931f58c77f4eff41d5108c5bb2c993033a
David Romero <davromalc@gmail.com>
2018-05-29 16:04:54 +0200

    DEVB-001: Added cron expression management capabilities. Now, the process is executed as many times as we configured in the cron expression.

--- collector/src/main/java/com/devbunch/feedcollector/FeedCollectorApplication.java
+++ collector/src/main/java/com/devbunch/feedcollector/FeedCollectorApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FeedCollectorApplication {
 
-  public static void main(String[] args) {
-    System
-        .exit(SpringApplication.exit(SpringApplication.run(FeedCollectorApplication.class, args)));
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(FeedCollectorApplication.class, args);
+	}
 }
