88660b2dce8de971574d697375cc6f0c1f7ed2a9
ivangrod <ivangutrod@gmail.com>
2018-07-30 12:26:34 +0200

    DEVB-011 - Added dis_max query for using multiple fields at the search

--- search-api/src/main/kotlin/com/devbunch/searchapi/controller/SearchController.kt
+++ search-api/src/main/kotlin/com/devbunch/searchapi/controller/SearchController.kt
@@ -1,8 +1,10 @@
 package com.devbunch.searchapi.controller
 
-import com.google.gson.JsonObject
+import com.devbunch.searchapi.model.FeedItem
 import io.searchbox.client.JestClient
 import io.searchbox.core.Search
+import io.searchbox.core.SearchResult
+import org.elasticsearch.index.query.DisMaxQueryBuilder
 import org.elasticsearch.index.query.QueryBuilders
 import org.elasticsearch.search.builder.SearchSourceBuilder
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,8 +12,6 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import io.searchbox.core.SearchResult
-import com.devbunch.searchapi.model.FeedItem
 
 @RestController
 class SearchController {
@@ -23,21 +23,21 @@ class SearchController {
 	private var indexName: String? = "feed-item-complete"
 
 	@GetMapping("/search")
-	fun search(@RequestParam(value = "term", defaultValue = "*") term: String): List<SearchResult.Hit<FeedItem,Void>>? {
+	fun search(@RequestParam(value = "term", defaultValue = "*") term: String): List<SearchResult.Hit<FeedItem, Void>>? {
 
 		System.out.println("You want to seach: $term")
 
-		val searchSourceBuilder = SearchSourceBuilder();
-		searchSourceBuilder.query(QueryBuilders.matchQuery("title", term));
+		val searchSourceBuilder = SearchSourceBuilder()
+		searchSourceBuilder.query(QueryBuilders.disMaxQuery().add(QueryBuilders.termQuery("topics", term)).add(QueryBuilders.termQuery("title", term)))
 
 		val search: Search.Builder = Search.Builder(searchSourceBuilder.toString())
 		if (indexName != null) search.addIndex(indexName)
 
-		val result = client?.execute(search.build())				
+		val result = client?.execute(search.build())
 		val resultList = result?.getHits(FeedItem::class.java)
-		
+
 		System.out.println("found: ${resultList?.size}")
-		
+
 		return resultList
 	}
 }
