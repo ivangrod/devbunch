a4d1f97e1ce0c2584285742264eb40e734654b17
David Romero <davromalc@gmail.com>
2018-08-07 18:34:35 +0200

    Fixed stage elasticsearch host
    Fixed jar execution bug. Now generated jar is executable
    
    Fixed missing manifest in fat jar created with kotlin and maven
    
    Added environment resolution
    
    Fixed stage elasticsearch host

--- search-api/src/main/kotlin/com/devbunch/searchapi/SearchApiApplication.kt
+++ search-api/src/main/kotlin/com/devbunch/searchapi/SearchApiApplication.kt
@@ -3,10 +3,21 @@ package com.devbunch.searchapi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.core.env.StandardEnvironment
 
 @SpringBootApplication
-open class SearchApiApplication
+class SearchApiApplication
 
-fun main(args: Array<String>) {
-     SpringApplication.run(SearchApiApplication::class.java, *args)
-}
+	val DEFAULT_ENVIRONMENT = "local"
+
+	fun main(args: Array<String>) {
+	     SpringApplicationBuilder(SearchApiApplication::class.java).environment(object : StandardEnvironment() {
+
+			public override fun getActiveProfiles() : Array<String> {
+				var systemEnvironmentVar: String? = System.getenv("ENV")
+				val environmentStr = if ( systemEnvironmentVar == null ) DEFAULT_ENVIRONMENT else systemEnvironmentVar
+				return arrayOf(environmentStr)
+			}
+		}).run(*args)
+	}
