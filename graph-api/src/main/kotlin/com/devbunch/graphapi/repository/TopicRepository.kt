0203be4cae52c02ddf2592d6b0bfb8f3c0b99528
David Romero <davromalc@gmail.com>
2018-10-18 20:33:03 +0200

    Revert "Initial development"
    This reverts commit 117ba76e23601d420bd1ffd410bbc4e521eee235.

--- graph-api/src/main/kotlin/com/devbunch/graphapi/repository/TopicRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.devbunch.graphapi.repository
-
-import com.devbunch.graphapi.model.Topic
-import org.springframework.data.repository.CrudRepository
-
-interface TopicRepository : CrudRepository<Topic, Long> {
-}
